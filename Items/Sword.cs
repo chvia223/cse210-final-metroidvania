5e9bdf10dc46fee0bc62c875c0fed3464babca99
Christian Viazzo <via17001@byui.edu>
2021-11-27 20:22:28 -0700

    initial Sword class

--- Items/Sword.cs
+++ Items/Sword.cs
@@ -5,15 +5,11 @@ namespace cse210_final_metroidvania.Casting
     /// <summary>
     /// Base class for all actors in the game.
     /// </summary>
-    public class Hero : Actor
+    public class Sword
     {
-        public Hero()
+        public Sword()
         {
-            SetHeight(Constants.HERO_HEIGHT);
-            SetWidth(Constants.HERO_WIDTH);
-            SetImage(Constants.IMAGE_HERO);
-            SetPosition(new Point(100, 100));
-            SetVelocity(new Point(0, Constants.GRAVITY));
+            
         }
 
     }
