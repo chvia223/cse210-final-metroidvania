3f0ee449a09fd386510d3ad9b43d07f468479f02
Christian Viazzo <via17001@byui.edu>
2021-11-27 20:19:18 -0700

    renamed relevant constants to hero and added gravity

--- Constants.cs
+++ Constants.cs
@@ -16,8 +16,8 @@ namespace cse210_final_metroidvania
         public const int DEFAULT_TEXT_OFFSET = 4;
 
         public const string IMAGE_BRICK = "./Assets/brick-3.png";
-        public const string IMAGE_PADDLE = "./Assets/bat.png";
-        public const string IMAGE_BALL = "./Assets/ball.png";
+        public const string IMAGE_FLOOR = "./Assets/floor_temp.png";
+        public const string IMAGE_HERO = "./Assets/hero_temp.png";
 
         public const string SOUND_START = "./Assets/start.wav";
         public const string SOUND_BOUNCE = "./Assets/boing.wav";
@@ -32,18 +32,18 @@ namespace cse210_final_metroidvania
         public const int PADDLE_X = MAX_X / 2;
         public const int PADDLE_Y = MAX_Y - 25;
 
-        public const int BRICK_WIDTH = 48;
-        public const int BRICK_HEIGHT = 24;
-
-        public const int BRICK_SPACE = 5;
+        public const int FLOOR_WIDTH = 50;
+        public const int FLOOR_HEIGHT = 50;
 
         public const int PADDLE_SPEED = 15;
 
         public const int PADDLE_WIDTH = 96;
         public const int PADDLE_HEIGHT = 24;
 
-        public const int BALL_WIDTH = 24;
-        public const int BALL_HEIGHT = 24;
+        public const int HERO_WIDTH = 24;
+        public const int HERO_HEIGHT = 24;
+
+        public const int GRAVITY = 10;
     }
 
 }
